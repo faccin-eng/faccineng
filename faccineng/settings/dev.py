6012eb86a8bdb3324cf9b136f880f0c1bf085797
faccin-eng <mjfaccin@hotmail.com>
2025-12-27 11:36:44 -0400

    first commit

---
+++ faccineng/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-w$0i#9kvwn11$al&-hc*-(vk)y8v@$--fqkm-_+w+)%+vt1erk"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
