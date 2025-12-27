68f2caa4b1bc9afd69a96a5f95079051b2dce05c
faccin-eng <mjfaccin@hotmail.com>
2025-12-27 11:46:02 -0400

    apps e attribs

---
+++ pages/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from wagtail.models import Page
+from wagtail.fields import StreamField
+from wagtail import blocks
+from wagtail.admin.panels import FieldPanel
+
+class StandardPage(Page):
+    body = StreamField([
+        ('heading', blocks.CharBlock()),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+    ], use_json_field=True)
+    
+    content_panels = Page.content_panels + [
+        FieldPanel('body'),
+    ]
