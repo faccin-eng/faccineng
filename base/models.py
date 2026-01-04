756c3ff9543d01b98e8f1806060c0637ce94a1ab
faccin-eng <mjfaccin@hotmail.com>
2026-01-04 13:37:31 -0400

    banner carousel

--- base/models.py
+++ base/models.py
@@ -28,25 +28,10 @@ class FooterSettings(BaseGenericSetting):
         verbose_name="Banner 1 (800x110px)"
     )
     
-    banner_image2 = models.ForeignKey(
-        'wagtailimages.Image',
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='+',
-        verbose_name="Banner 2 (opcional)"
-    )
-
     banner_url1 = models.URLField(
         blank=True,
         verbose_name="Link Imagem 1",
         help_text="URL opcional da imagem 1)"
-    )
-
-    banner_url2 = models.URLField(
-        blank=True,
-        verbose_name="Link Imagem 2", 
-        help_text="URL opcional da imagem 2)"
     )
 
     footer_text = RichTextField(blank=True, verbose_name="Texto do Rodapé")
@@ -63,10 +48,60 @@ class FooterSettings(BaseGenericSetting):
 
         MultiFieldPanel([
             FieldPanel("banner_image1"),
-            FieldPanel("banner_image2"),
             FieldPanel("banner_url1"),
-            FieldPanel("banner_url2"),
         ], "Configurações do carrossel de banners do Rodapé (800x110px)"),
         FieldPanel("footer_text"),
     ]
+@register_setting
+class HeaderSettings(BaseGenericSetting):
+    banner_image1 = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        verbose_name="Banner 1 (800x110px)"
+    )
+    banner_image2 = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        verbose_name="Banner 2 (800x110px)"
+    )
+    banner_image3 = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        verbose_name="Banner 3 (800x110px)"
+    )
+    
+    banner_url1 = models.URLField(
+        blank=True,
+        verbose_name="Link Imagem 1",
+        help_text="URL opcional da imagem 1)"
+    )
+    banner_url2 = models.URLField(
+        blank=True,
+        verbose_name="Link Imagem 2",
+        help_text="URL opcional da imagem 2)"
+    )
+    banner_url3 = models.URLField(
+        blank=True,
+        verbose_name="Link Imagem 3",
+        help_text="URL opcional da imagem 3)"
+    )
 
+    panels = [
+        MultiFieldPanel([
+            FieldPanel("banner_image1"),
+            FieldPanel("banner_image2"),
+            FieldPanel("banner_image3"),
+            FieldPanel("banner_url1"),
+            FieldPanel("banner_url2"),
+            FieldPanel("banner_url3"),
+        ], "Configurações do carrossel de banners do Rodapé (800x110px)"),
+    ]
