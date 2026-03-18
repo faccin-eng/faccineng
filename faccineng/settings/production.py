6d8fafc42e8499be0d78ba578ea0be31b5d4da18
faccin-eng <mjfaccin@hotmail.com>
2026-03-18 12:09:15 -0400

    reprod

--- faccineng/settings/production.py
+++ faccineng/settings/production.py
@@ -3,6 +3,7 @@ from .base import *
 DEBUG = False
 
 ALLOWED_HOSTS = ['faccinengenharia.com', 'www.faccinengenharia.com', '68.183.24.170']
+SECRET_KEY = '9n85c(yr8ugg7j_mvv24ah76un@lk8o*c#uios6cdg+orsr)2@'
 
 # ManifestStaticFilesStorage is recommended in production, to prevent
 # outdated JavaScript / CSS assets being served from cache
